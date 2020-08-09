390fbefb3a83d7c55724cd0104ff06dc756da91f
sabeerzabi <sabeerzabi@gmail.com>
2020-08-10 00:09:05 +0530

    image lossy compression

--- images/small/webp.sh
+++ images/small/webp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # converting JPEG images
-find $1 -type f -and \( -iname "*.jpg" -o -iname "*.jpeg" \) \
+find $1 -type f -and \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) \
 -exec bash -c '
 webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
 if [ ! -f "$webp_path" ]; then
   cwebp -quiet -q 90 "$0" -o "$webp_path";
 fi;' {} \;
 
-# converting PNG images
+# converting PNG images lossless, lossy
 find $1 -type f -and -iname "*.png" \
 -exec bash -c '
 webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
 if [ ! -f "$webp_path" ]; then
-  cwebp -quiet -lossless "$0" -o "$webp_path";
+  cwebp -quiet -lossy "$0" -o "$webp_path";
 fi;' {} \;
--- images/webp.sh
+++ images/webp.sh
@@ -8,10 +8,10 @@ if [ ! -f "$webp_path" ]; then
   cwebp -quiet -q 90 "$0" -o "$webp_path";
 fi;' {} \;
 
-# converting PNG images
+# converting PNG images lossless, lossy
 find $1 -type f -and -iname "*.png" \
 -exec bash -c '
 webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
 if [ ! -f "$webp_path" ]; then
-  cwebp -quiet -lossless "$0" -o "$webp_path";
+  cwebp -quiet -lossy "$0" -o "$webp_path";
 fi;' {} \;
