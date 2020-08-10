9646c8700ae2da6a024abd9bb4b08367f2f9c605
sabeerzabi <sabeerzabi@gmail.com>
2020-08-10 19:39:49 +0530

    preloader added with opacity background

--- asset/custom.js
+++ asset/custom.js
@@ -4,7 +4,7 @@ $(window).on('load', function() {
     /*=========================================================================
         Preloader
     =========================================================================*/
-    $(".module-loading").delay(350).removeClass('d-none');
+    $("#preloader").delay(350).fadeOut();
     // Because only Chrome supports offset-path, feGaussianBlur for now
     var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
