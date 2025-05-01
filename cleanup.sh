ec45237adb01cc92c3ad3b740b01d241cd24c0d3
Sabeer C A <sabeerzabi@gmail.com>
2025-05-02 03:38:37 +0530

    Add twitter image

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,7 @@
 shopt -s extglob dotglob
 
 # Remove all files and directories except the allowed ones.
-rm -rf -- !("cleanup.sh"|"Backup"|"dist"|".gitignore"|".git"|"node_modules")
+rm -rf -- !("cleanup.sh"|"Backup"|"dist"|".gitignore"|".git"|"node_modules"|"music")
 
 # If the 'dist' directory exists, move all its files to the current directory
 if [ -d "dist" ]; then
